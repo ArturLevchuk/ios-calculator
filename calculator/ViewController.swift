34d85a0853cfc08243f2dde4c5878cdfb6526fdc
ArturLevchuk <levchukarturr@gmail.com>
2024-01-22 18:30:52 +0200

    Initial Commit

---
+++ calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Artur Levchuk on 22.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
